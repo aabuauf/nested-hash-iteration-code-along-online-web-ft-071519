21676dffa3d5595874a30673255ef7b7d6782671
Ahmed Abououf <aabuauf@yahoo.com>
2019-07-17 19:30:02 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,6 +15,7 @@ require 'pry'
    }
   
 def remove_strawberry(contacts)
-contacts["Freddy Mercury"][:favorite_ice_cream_flavors].shift()
-return contacts
+contacts.each do |person, contact_details_hash|
+  
+end
 end
